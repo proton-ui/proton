fe4d5f8a4ae48bc56f738accfbdced1d6134924d
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2019-10-10 16:54:02 -0700

    Add initial autocomplete and tags components

---
+++ src/components/Tags/RenderlessTags.vue
@@ -0,0 +1,54 @@
+<script>
+    export default {
+        name: 'p-renderless-tags',
+
+        data() {
+            return {
+                newTag: '',
+            }
+        },
+
+        props: ['value'],
+
+        methods: {
+            addTag() {
+                if (this.newTag.trim().length === 0 || this.value.includes(this.newTag.trim())) {
+                    return
+                }
+
+                this.$emit('input', [...this.value, this.newTag.trim()])
+                this.newTag = ''
+            },
+
+            removeTag(tag) {
+                this.$emit('input', this.value.filter(t => t !== tag))
+            },
+        },
+
+        render() {
+            return this.$scopedSlots.default({
+                tags: this.value,
+                available: this.available,
+                addTag: this.addTag,
+                removeTag: this.removeTag,
+                
+                inputAttrs: {
+                    value: this.newTag,
+                },
+
+                inputEvents: {
+                    input: (e) => {
+                        this.newTag = e.target.value
+                    },
+
+                    keydown: (e) => {
+                        if (e.keyCode === 13) {
+                            e.preventDefault()
+                            this.addTag()
+                        }
+                    }
+                }
+            })
+        },
+    }
+</script>
